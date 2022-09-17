4803329a3f102f46d1fe863befe66ac0aab0d6a0
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2022-09-17 16:52:08 +0700

    feat(Collection): :sparkles: Add more methods.
    Add methods to create linked and tree map-set. Add methods to make list unique. Add methods to combine two list.

--- src/main/java/ananta/utility/ACollection.java
+++ src/main/java/ananta/utility/ACollection.java
@@ -6,7 +6,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import static java.util.function.Predicate.not;
 
 // TODO: Update class document.
 /**
@@ -36,11 +39,43 @@ public final class ACollection {
     }
     
     /**
+     * @return a modifiable empty linked set.
+     */
+    @NotNull
+    public static <T> Set<T> emptyLinkedSet() {
+        return new LinkedHashSet<>();
+    }
+    
+    /**
+     * @return a modifiable empty tree set.
+     */
+    @NotNull
+    public static <T> Set<T> emptyTreeSet() {
+        return new TreeSet<>();
+    }
+    
+    /**
      * @return a modifiable empty map.
      */
     @NotNull
     public static <K, V> Map<K, V> emptyMap() {
         return new HashMap<>();
+    }
+    
+    /**
+     * @return a modifiable empty linked map.
+     */
+    @NotNull
+    public static <K, V> Map<K, V> emptyLinkedMap() {
+        return new LinkedHashMap<>();
+    }
+    
+    /**
+     * @return a modifiable empty tree map.
+     */
+    @NotNull
+    public static <K, V> Map<K, V> emptyTreeMap() {
+        return new TreeMap<>();
     }
     
     /**
@@ -64,6 +99,24 @@ public final class ACollection {
     }
     
     /**
+     * @return a modifiable list that contains input value with no duplicated.
+     */
+    @SafeVarargs
+    @NotNull
+    public static <T> List<T> uniqueListOf(@Nullable final T... elements) {
+        return uniqueListOf(listOf(elements));
+    }
+    
+    /**
+     * Get all unique items of other collections and put it into a list.
+     * @return a modifiable list that contains all items of other collection.
+     */
+    @NotNull
+    public static <T> List<T> uniqueListOf(final @Nullable Collection<T> list) {
+        return emptyListIfNull(list).stream().distinct().collect(Collectors.toList());
+    }
+    
+    /**
      *
      * @return a modifiable list that contains all non-null input value.
      */
@@ -84,6 +137,29 @@ public final class ACollection {
         return isEmpty(collection)
             ? emptyList()
             : collection.stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
+    
+    /**
+     *
+     * @return a modifiable list that contains all non-null and unique input value.
+     */
+    @SafeVarargs
+    @NotNull
+    public static <T> List<T> uniqueListNonNullOf(@Nullable final T... elements) {
+        return uniqueListNonNullOf(listOf(elements));
+    }
+    
+    /**
+     * Get all non-null and unique items of other collections and put it into a list.
+     * @return a modifiable list that contains all non-null and unique items of other collection.
+     */
+    @NotNull
+    public static <T> List<T> uniqueListNonNullOf(final @Nullable Collection<T> collection) {
+        return emptyListIfNull(collection)
+            .stream()
+            .filter(Objects::nonNull)
+            .distinct()
+            .collect(Collectors.toList());
     }
     
     /**
@@ -122,6 +198,78 @@ public final class ACollection {
         return isEmpty(collection)
             ? emptySet()
             : collection.stream().filter(Objects::nonNull).collect(Collectors.toSet());
+    }
+    
+    /**
+     * @return a modifiable linked set that contains all input value.
+     */
+    @SafeVarargs
+    @NotNull
+    public static <T> Set<T> linkedSetOf(@Nullable final T... items) {
+        return new LinkedHashSet<>(Arrays.asList(items));
+    }
+    
+    /**
+     * Get all items of other collections and put it into a linked set.
+     * @return a modifiable set that contains all items of other collection.
+     */
+    @NotNull
+    public static <T> Set<T> linkedSetOf(@Nullable final Collection<T> collection) {
+        return isEmpty(collection) ? emptyLinkedSet() : new LinkedHashSet<>(collection);
+    }
+    
+    /**
+     * @return a modifiable linked set that contains all input value.
+     */
+    @SafeVarargs
+    @NotNull
+    public static <T> Set<T> linkedSetNonNullOf(@Nullable final T... items) {
+        return linkedSetOf(listNonNullOf(items));
+    }
+    
+    /**
+     * Get all items of other collections and put it into a linked set.
+     * @return a modifiable set that contains all items of other collection.
+     */
+    @NotNull
+    public static <T> Set<T> linkedSetNonNullOf(@Nullable final Collection<T> collection) {
+        return linkedSetOf(listNonNullOf(collection));
+    }
+    
+    /**
+     * @return a modifiable tree set that contains all input value.
+     */
+    @SafeVarargs
+    @NotNull
+    public static <T> Set<T> treeSetOf(@Nullable final T... items) {
+        return new TreeSet<>(Arrays.asList(items));
+    }
+    
+    /**
+     * Get all items of other collections and put it into a tree set.
+     * @return a modifiable tree set that contains all items of other collection.
+     */
+    @NotNull
+    public static <T> Set<T> treeSetOf(@Nullable final Collection<T> collection) {
+        return isEmpty(collection) ? emptyTreeSet() : new TreeSet<>(collection);
+    }
+    
+    /**
+     * @return a modifiable tree set that contains all input value.
+     */
+    @SafeVarargs
+    @NotNull
+    public static <T> Set<T> treeSetNonNullOf(@Nullable final T... items) {
+        return treeSetOf(listNonNullOf(items));
+    }
+    
+    /**
+     * Get all items of other collections and put it into a tree set.
+     * @return a modifiable tree set that contains all items of other collection.
+     */
+    @NotNull
+    public static <T> Set<T> treeSetNonNullOf(@Nullable final Collection<T> collection) {
+        return treeSetOf(listNonNullOf(collection));
     }
     
     /**
@@ -300,5 +448,316 @@ public final class ACollection {
         }
         return Optional.ofNullable(list.get(list.size() - 1));
     }
-
+    
+    /**
+     * Move a element in a list to a certain index.
+     * If new index is smaller than 0 or larger than the size of list,
+     * this method will return the origin list.
+     * @param index new index of the move element. Can be null.
+     * @param element element that you want to move. Can be null.
+     * @param list list contains element you want to move. Can be empty.
+     * @return empty list if input list is null.
+     * Return input list if element is not found or the index is lower than 0 or larger than size of the list.
+     * Otherwise, return the moved list.
+     */
+    @NotNull
+    public static <T> List<T>  moveElementTo(final int index, @Nullable T element, @Nullable List<T> list) {
+        if (isEmpty(list) || element == null || index < 0 || index >= sizeOf(list)) {
+            return listOf(list);
+        }
+        List<T> result = list.stream().filter(item_ -> !Objects.equals(item_, element)).collect(Collectors.toList());
+        result.add(index, element);
+    
+        return result;
+    }
+    
+    /**
+     * Move a element to the head of a list.
+     * @param element element that you want to move. Can be null.
+     * @param list a list that contains the element. Can be null.
+     * @return empty list if input list is null. Return input list if the list doesn't contains the element.
+     * Otherwise, return the list in which element is in the head.
+     */
+    @NotNull
+    public static <T> List<T>  moveElementToHead(@Nullable T element, @Nullable List<T> list) {
+        return moveElementTo(0, element, list);
+    }
+    
+    /**
+     * Move a element to the tail of a list.
+     * @param element element that you want to move. Can be null.
+     * @param list a list that contains the element. Can be null.
+     * @return empty list if input list is null. Return input list if the list doesn't contains the element.
+     * Otherwise, return the list in which element is in the tail.
+     */
+    @NotNull
+    public static <T> List<T>  moveElementToTail(@Nullable T element, @Nullable List<T> list) {
+        return moveElementTo(sizeOf(list) - 1, element, list);
+    }
+    
+    /**
+     * Concatenate two list into a new list.
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A, A, B, B, null, B, B, C, C, null]
+     * </pre>
+     * @param head the begin of the new list. Can be null.
+     * @param tail the end of the new list. Can be null.
+     * @return new merged list of 2 input list.
+     */
+    @NotNull
+    public static <T> List<T> concat(@Nullable List<T> head, @Nullable List<T> tail) {
+        if (isEmpty(head)) {
+            return listOf(tail);
+        }
+        if (isEmpty(tail)) {
+            return listOf(head);
+        }
+        List<T> result = listOf(head);
+        result.addAll(tail);
+        return result;
+    }
+    
+    /**
+     * Concatenate two list into a new list. The return list will contains only unique elements.
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A, B, null, C]
+     * </pre>
+     * @param head the begin of the new list. Can be null.
+     * @param tail the end of the new list. Can be null.
+     * @return new unique merged list of 2 input list.
+     */
+    @NotNull
+    public static <T> List<T> uniqueConcat(@Nullable List<T> head, @Nullable List<T> tail) {
+        if (isEmpty(head)) {
+            return uniqueListOf(tail);
+        }
+        if (isEmpty(tail)) {
+            return uniqueListOf(head);
+        }
+        List<T> result = emptyListIfNull(head);
+        result.addAll(tail);
+        return uniqueListOf(result);
+    }
+    
+    /**
+     * Merge two list into a new list.
+     * This only merge those unique items (items which are not existed in the other one).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A, A, B, B, null, C, C]
+     * </pre>
+     * @param head the begin of the new list. Can be null.
+     * @param tail the end of the new list. Can be null.
+     * @return new merged list of 2 input list.
+     */
+    @NotNull
+    public static <T> List<T> merge(@Nullable List<T> head, @Nullable List<T> tail) {
+        if (isEmpty(head)) {
+            return listOf(tail);
+        }
+        if (isEmpty(tail)) {
+            return listOf(head);
+        }
+        
+        List<T> headList = listOf(head);
+        Set<T> headSet = setOf(head);
+        
+        List<T> itemsThatNotExistedInHead = tail.stream().filter(item -> !headSet.contains(item)).collect(Collectors.toList());
+        headList.addAll(itemsThatNotExistedInHead);
+        
+        return headList;
+    }
+    
+    /**
+     * Get a list contains all elements existing in both list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [B, B, null, B, B, null]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> innerJoin(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> leftSet = setOf(left);
+        Set<T> rightSet = setOf(right);
+        
+        Predicate<T> elementsInBothList = element -> leftSet.contains(element) && rightSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(elementsInBothList)
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all unique elements existing in both list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [B, null]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> uniqueInnerJoin(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> leftSet = setOf(left);
+        Set<T> rightSet = setOf(right);
+        
+        Predicate<T> elementsInBothList = element -> leftSet.contains(element) && rightSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(elementsInBothList)
+            .distinct()
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all elements existing in only the left list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A, A]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> leftExcludeJoin(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> leftSet = setOf(left);
+        Set<T> rightSet = setOf(right);
+        
+        Predicate<T> elementsInLeftOnly = element -> leftSet.contains(element) && !rightSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(elementsInLeftOnly)
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all unique elements existing in only the left list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> uniqueLeftExcludeJoin(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> leftSet = setOf(left);
+        Set<T> rightSet = setOf(right);
+    
+        Predicate<T> elementsInLeftOnly = element -> leftSet.contains(element) && !rightSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(elementsInLeftOnly)
+            .distinct()
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all elements existing in only the right list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [C, C]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> rightExcludeJoin(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> leftSet = setOf(left);
+        Set<T> rightSet = setOf(right);
+        
+        Predicate<T> elementsInRightOnly = element -> !leftSet.contains(element) && rightSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(elementsInRightOnly)
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all unique elements existing in only the right list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [C]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> uniqueRightExcludeJoin(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> leftSet = setOf(left);
+        Set<T> rightSet = setOf(right);
+    
+        Predicate<T> elementsInRightOnly = element -> !leftSet.contains(element) && rightSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(elementsInRightOnly)
+            .distinct()
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all unique elements existing in only the right list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A, A, C, C]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> different(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> listSet = setOf(left);
+        Set<T> otherSet = setOf(right);
+        
+        Predicate<T> inBothList = element -> listSet.contains(element) && otherSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(not(inBothList))
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get a list contains all unique elements existing in only the right list (including null).
+     * <pre> Example:
+     * - left: [A, A, B, B, null]
+     * - right: [B, B, C, C, null]
+     * - return: [A, C]
+     * </pre>
+     * @param left first list, can be null.
+     * @param right second list, can be null.
+     */
+    @NotNull
+    public static <T> List<T> uniqueDifferent(@Nullable List<T> left, @Nullable List<T> right) {
+        Set<T> listSet = setOf(left);
+        Set<T> otherSet = setOf(right);
+        
+        Predicate<T> inBothList = element -> listSet.contains(element) && otherSet.contains(element);
+        
+        return concat(left, right)
+            .stream()
+            .filter(not(inBothList))
+            .distinct()
+            .collect(Collectors.toList());
+    }
 }
