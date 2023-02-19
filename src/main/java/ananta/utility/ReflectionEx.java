ce6eb8875ff43f380523a1b1f5a108468befb839
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2023-02-19 12:26:11 +0700

    feat(Reflection): :sparkles: Add more methods related to check annotations.

--- src/main/java/ananta/utility/ReflectionEx.java
+++ src/main/java/ananta/utility/ReflectionEx.java
@@ -4,10 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,7 +15,7 @@ import java.util.stream.Stream;
  * This class provides some methods related to reflection.
  * Most methods can handle NULL input well.
  */
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "UnusedReturnValue"})
 public final class ReflectionEx {
 
     private final static Map<String, Class<?>> primitiveWrapperClassMap;
@@ -43,21 +40,22 @@ public final class ReflectionEx {
     private static final Set<Class<?>> wrapperClasses = SetEx.setOf(
         Boolean.class, Byte.class, Character.class, Double.class, Float.class, Integer.class, Long.class, Short.class, Void.class
     );
-    
+
     /**
-     * Find field's value of a object.
-     * @param field field of the object. Field can be null.
+     * Find field's value of an object.
+     *
+     * @param field  field of the object. Field can be null.
      * @param object object that contains the value. Object can be null.
      * @return Optional.empty() if input is null or field can not be accessed. Otherwise, return Optional of object.
      */
     @NotNull
-    public static Optional<?> getFieldValue(@Nullable final Field field, @Nullable final Object object) {
-        if (field == null || object == null){
+    public static Optional<?> getFieldValue(@Nullable final Object object, @Nullable final Field field) {
+        if (field == null || object == null) {
             return Optional.empty();
         }
-        
+
         final boolean canAccess = field.canAccess(object);
-        
+
         field.setAccessible(true);
         Object value = null;
         try {
@@ -66,22 +64,48 @@ public final class ReflectionEx {
             e.printStackTrace();
         }
         field.setAccessible(canAccess);
-        
+
         return Optional.ofNullable(value);
     }
 
     /**
-     * Find field's value of a object.
-     * @param fieldName field name that you want to extract. Field name should not be be null.
-     * @param object object that contains the value. Object should not be be null.
+     * Find field's value of an object.
+     * <pre>
+     * Result example:
+     *      - Object:
+     *      {
+     *          "father": {
+     *              "child": "Hello world".
+     *          }
+     *      }
+     *      - Field names: "father" -> Result: { "child": "Hello world". }
+     *      - Field names: "father.child" -> Result: "Hello world"
+     * </pre>
+     *
+     * @param object     object that contains the value. Object should not be null.
+     * @param fieldNames fields name that you want to extract.
+     *                   If you want to get nested field, please use comma between those nested fields.
+     *                   Field name should not be null.
      * @return Optional.empty() if input is null or field can not be accessed. Otherwise, return Optional of object.
      */
-    @NotNull
-    public static Optional<?> getFieldValue(@Nullable final String fieldName, @Nullable final Object object) {
-        if (fieldName == null || object == null){
+    public static Optional<Object> getFieldValue(@Nullable final Object object, @Nullable final String fieldNames) {
+        if (object == null || fieldNames == null) {
             return Optional.empty();
         }
-        return findField(fieldName, object.getClass()).flatMap(field -> getFieldValue(field, object));
+        final String[] fieldNameList = fieldNames.split("\\.");
+        Object result = object;
+        for (final String field : fieldNameList) {
+            result = getFieldValue0(field, result).orElse(null);
+        }
+        return Optional.ofNullable(result);
+    }
+
+    @NotNull
+    private static Optional<?> getFieldValue0(@Nullable final String fieldName, @Nullable final Object object) {
+        if (fieldName == null || object == null) {
+            return Optional.empty();
+        }
+        return findField(fieldName, object.getClass()).flatMap(field -> getFieldValue(object, field));
     }
 
     /**
@@ -133,10 +157,24 @@ public final class ReflectionEx {
         final List<Class<?>> classes = ancestorClassesOf(clazz);
         return classes.stream().map(Class::getDeclaredFields).flatMap(Stream::of).collect(Collectors.toList());
     }
-    
+
+    /**
+     * Find all static fields of certain classes. Those classes include ones from parent classes.
+     *
+     * @param clazz Class that you want to find static fields from.
+     * @return Empty list if class passed is null. Otherwise, return static fields of class and its parent classes.
+     */
+    public static List<Field> staticFieldsOf(@Nullable final Class<?> clazz) {
+        if (clazz == null) {
+            return ListEx.emptyList();
+        }
+        return ReflectionEx.fieldsOf(clazz).stream().filter(ReflectionEx::isStaticField).collect(Collectors.toUnmodifiableList());
+    }
+
     /**
      * Find all non-static fields of a class. Those fields including private and public fields in
      * that one class and its ancestors.
+     *
      * @param clazz The class that you want to extract. Class can be null.
      * @return empty list if class is null, otherwise return its fields.
      */
@@ -381,7 +419,7 @@ public final class ReflectionEx {
     }
 
     /**
-     * Cast a object to other type.
+     * Cast an object to other type.
      * @param clazz class that we want to cast to. must be not null.
      * @param object object that we want to cast. should be not null.
      * @return empty if input is null or cast failed. Otherwise, return casted value.
@@ -399,7 +437,7 @@ public final class ReflectionEx {
     }
 
     /**
-     * Cast a object to other type.
+     * Cast an object to other type.
      * @param clazz class that we want to cast to. must be not null.
      * @param object object that we want to cast. should be not null.
      * @return empty if input is null or cast failed. Otherwise, return casted value.
@@ -439,40 +477,187 @@ public final class ReflectionEx {
         }
         return wrapperClass;
     }
-    
+
     //=================================================// Checkers //=================================================//
     /**
+     * Check if a class is static class or not.
+     *
+     * @param clazz Class that you want to check
+     * @return False if class is null or is not static class. Otherwise, return true.
+     */
+    public static boolean isStaticClass(@Nullable final Class<?> clazz) {
+        if (clazz == null) {
+            return false;
+        }
+        return Modifier.isStatic(clazz.getModifiers());
+    }
+
+    /**
+     * Check if a method is static method or not.
+     *
+     * @param method Method that you want to check
+     * @return False if method is null or is not static method. Otherwise, return true.
+     */
+    public static boolean isStaticMethod(@Nullable final Method method) {
+        if (method == null) {
+            return false;
+        }
+        return Modifier.isStatic(method.getModifiers());
+    }
+
+    /**
+     * Check if a field is static field or not.
+     *
+     * @param field Field that you want to check
+     * @return False if field is null or is not static field. Otherwise, return true.
+     */
+    public static boolean isStaticField(@Nullable final Field field) {
+        if (field == null) {
+            return false;
+        }
+        return Modifier.isStatic(field.getModifiers());
+    }
+
+    /**
      * Check if a class has annotation or not.
+     *
+     * @param clazz           can be null.
      * @param annotationClass can be null.
-     * @param clazz can be null.
      * @return false if input is null or class doesn't have annotation. Otherwise, return true.
      */
-    public static boolean hasAnnotation(@Nullable final Class<? extends Annotation> annotationClass, @Nullable final Class<?> clazz) {
+    public static boolean isAnnotatedOn(@Nullable final Class<?> clazz, @Nullable final Class<? extends Annotation> annotationClass) {
         if (annotationClass == null || clazz == null) {
             return false;
         }
         final List<Annotation> annotations = annotationsOf(clazz);
         return annotations.stream().anyMatch(annotation -> annotation.annotationType().equals(annotationClass));
     }
-    
+
     /**
      * Check if certain field contains the annotation or not.
+     *
+     * @param field           can be null.
      * @param annotationClass can be null.
-     * @param field can be null.
      * @return false if any input is null or field not contains annotation. Otherwise, return true.
      */
-    public static boolean hasAnnotation(@Nullable final Class<? extends Annotation> annotationClass, @Nullable final Field field) {
+    public static boolean isAnnotatedOn(@Nullable final Field field, @Nullable final Class<? extends Annotation> annotationClass) {
         if (annotationClass == null || field == null) {
             return false;
         }
         return field.getAnnotation(annotationClass) != null;
     }
-    
+
+    /**
+     * Check if a field contains some annotations or not.
+     *
+     * @param method            Method that you want to check. Should not be null.
+     * @param annotationClasses Annotation classes that you want to check if present is method.
+     * @return False when:<p>
+     * - Method is null.</p><p>
+     * - No annotation passed down to method.</p><p>
+     * - No required annotation found in method.</p><p>
+     * Otherwise, return true.
+     * </p>
+     */
+    @SafeVarargs
+    public static boolean isAnnotatedOn(
+        @Nullable final Method method,
+        @Nullable final Class<? extends Annotation>... annotationClasses
+    ) {
+        if (method == null || annotationClasses == null || annotationClasses.length == 0) {
+            return false;
+        }
+        if (annotationClasses.length == 1) {
+            return method.isAnnotationPresent(annotationClasses[0]);
+        }
+        return CollectionEx.hasAnyOf(ListEx.listOf(annotationClasses), ListEx.listOf(method.getAnnotations()));
+    }
+
+    /**
+     * Check if a field contains some annotations or not.
+     *
+     * @param method            Method that you want to check. Should not be null.
+     * @param annotationClasses Annotation classes that you want to check if present is method.
+     * @return False when:<p>
+     * - Method is null.</p><p>
+     * - No annotation passed down to method.</p><p>
+     * - No required annotation found in method.</p><p>
+     * Otherwise, return true.
+     * </p>
+     */
+    public static boolean isAnnotatedOn(
+        @Nullable final Method method,
+        @Nullable final Collection<Class<? extends Annotation>> annotationClasses
+    ) {
+        if (method == null || CollectionEx.isEmpty(annotationClasses)) {
+            return false;
+        }
+        final List<Class<? extends Annotation>> classes = Arrays
+            .stream(method.getAnnotations())
+            .map(Annotation::annotationType)
+            .collect(Collectors.toList());
+        return CollectionEx.hasAnyOf(annotationClasses, classes);
+    }
+
+    /**
+     * Check if a field contains some annotations or not.
+     *
+     * @param clazz             Class that you want to check. Should not be null.
+     * @param annotationClasses Annotation classes that you want to check if present is class.
+     * @return False when:<p>
+     * - Class is null.</p><p>
+     * - No annotation passed down to class.</p><p>
+     * - No required annotation found in class.</p><p>
+     * Otherwise, return true.
+     * </p>
+     */
+    @SafeVarargs
+    public static boolean isAnnotatedOn(
+        @Nullable final Class<?> clazz,
+        @Nullable final Class<? extends Annotation>... annotationClasses
+    ) {
+        if (clazz == null || annotationClasses == null || annotationClasses.length == 0) {
+            return false;
+        }
+        if (annotationClasses.length == 1) {
+            assert annotationClasses[0] != null;
+            return clazz.isAnnotationPresent(annotationClasses[0]);
+        }
+        return CollectionEx.hasAnyOf(ListEx.listOf(annotationClasses), ListEx.listOf(clazz.getAnnotations()));
+    }
+
+    /**
+     * Check if a field contains some annotations or not.
+     *
+     * @param clazz             Class that you want to check. Should not be null.
+     * @param annotationClasses Annotation classes that you want to check if present is class.
+     * @return False when:<p>
+     * - Class is null.</p><p>
+     * - No annotation passed down to class.</p><p>
+     * - No required annotation found in class.</p><p>
+     * Otherwise, return true.
+     * </p>
+     */
+    public static boolean isAnnotatedOn(
+        @Nullable final Class<?> clazz,
+        @Nullable final Collection<Class<? extends Annotation>> annotationClasses
+    ) {
+        if (clazz == null || CollectionEx.isEmpty(annotationClasses)) {
+            return false;
+        }
+        final List<Class<? extends Annotation>> classes = Arrays
+            .stream(clazz.getAnnotations())
+            .map(Annotation::annotationType)
+            .collect(Collectors.toList());
+        return CollectionEx.hasAnyOf(annotationClasses, classes);
+    }
+
     /**
      * Check if a field is exists in certain class or not.
      * This method will ignore case while checking.
+     *
      * @param fieldName can be null.
-     * @param clazz can be null.
+     * @param clazz     can be null.
      * @return false if inputs are null or when field is not exist in the class. Otherwise, return true.
      */
     public static boolean hasField(@Nullable final String fieldName, @Nullable final Class<?> clazz) {
